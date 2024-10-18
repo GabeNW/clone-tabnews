86bff14a9ce393e35e69f90455bc040a923cf2df
Gabe <152761431+GabeNW@users.noreply.github.com>
2024-10-18 00:03:19 +0000

    fix: Remover os arquivos desnecessários
    Deixar o repositório somente com os recursos necessários.
    
    Este commit remove todos os arquivos da pasta ".next" e "node_modules", além das pastas em si.
    Os arquivos foram removidos apenas no controle de versão, mas permanecendo seu diretório local (nesse caso, o codespace).

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home() {
-    return <h1>Mensagem feita pra minha mãe!!!
-        Te amo!!!
-    </h1>
+    return <h1>Mensagem feita pra minha mãe!!! Te amo!!!</h1>
+    
 }
 
 export default Home;
